04a1337bf8775e4ecacc008e642334d788968f1f
VipulChoudhary147 <54441715+VipulChoudhary147@users.noreply.github.com>
2021-03-29 15:46:52 +0530

    Create fibonacci series.py

---
+++ Vipul/fibonacci series.py
@@ -0,0 +1,13 @@
+def fib():
+  n = int(input("enter the nth position of series : "))
+  n1 = 0 
+  n2 =1 
+  for i in range(n):
+    if i <= 1:
+      sum = i
+    else:
+      sum = n1 + n2 
+      n1 = n2
+      n2 = sum
+    print(sum, end= ' ')
+fib()
