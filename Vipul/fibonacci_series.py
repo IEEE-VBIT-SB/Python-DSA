3379da67238b32290f9f8753b8e4d2f27876aad5
VipulChoudhary147 <54441715+VipulChoudhary147@users.noreply.github.com>
2021-03-30 14:09:32 +0530

    Update fibonacci_series.py

--- Vipul/fibonacci_series.py
+++ Vipul/fibonacci_series.py
@@ -1,11 +1,14 @@
-def fib(n):
-  n1 = 0 
-  n2 =1 
-  for i in range(n):
-    if i <= 1:
-      sum = i
+def fib(x):
+    n1 = 0 
+    n2 =1 
+    if n == 0 :
+        print("Inavlid input")
     else:
-      sum = n1 + n2 
-      n1 = n2
-      n2 = sum
-    print(sum, end= ' ')
+        for i in range(x):
+           if i <= 1:
+             sum = i
+           else:
+             sum = n1 + n2 
+             n1 = n2
+             n2 = sum
+           print(sum, end= ' ')
